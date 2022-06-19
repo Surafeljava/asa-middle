42733f7050fd2be23c31b8fdd4e1288184230c32
Surafeljava <surafelk27@gmail.com>
2022-06-19 22:06:33 +0300

    feat: middle ware done

--- app.py
+++ app.py
@@ -11,14 +11,40 @@ def getPrediction():
     return "Hello Prediction Middleware"
 
 
-@app.route('/predict', methods=['POST'])
-async def predict():
+@app.route('/predict_from_text', methods=['POST'])
+async def predict_from_text():
     try:
         data = request.json
         # sentence = data['sentence']
         # endpoint = f'/api/saved_tweets/create_analyzed_tweet'
         async with ClientSession(trust_env=True) as session:
             async with session.post(f'http://178.79.135.192/predict_from_text', json=data) as response:
+                return await response.json()
+
+    except Exception as e:
+        response = jsonify({'sentence': "sentence", 'error': e}), 400
+        return response
+
+
+@app.route('/predict_from_tweetid', methods=['POST'])
+async def predict_from_tweetid():
+    try:
+        data = request.json
+        async with ClientSession(trust_env=True) as session:
+            async with session.post(f'http://178.79.135.192/predict_from_tweetid', json=data) as response:
+                return await response.json()
+
+    except Exception as e:
+        response = jsonify({'sentence': "sentence", 'error': e}), 400
+        return response
+
+
+@app.route('/predict_from_username', methods=['POST'])
+async def predict_from_username():
+    try:
+        data = request.json
+        async with ClientSession(trust_env=True) as session:
+            async with session.post(f'http://178.79.135.192/predict_from_username', json=data) as response:
                 return await response.json()
 
     except Exception as e:
